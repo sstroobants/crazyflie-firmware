267c38f98e743a26f39fc4194fccf8e8b1454706
sstroobants <14180440+sstroobants@users.noreply.github.com>
2025-05-26 15:11:08 +0200

    apply configs

--- src/config/config.h
+++ src/config/config.h
@@ -101,6 +101,7 @@
 #define UART2_TASK_PRI            3
 #define CRTP_SRV_TASK_PRI         0
 #define PLATFORM_SRV_TASK_PRI     0
+#define TEENSY_TASK_PRI           3
 
 // Not compiled
 #if 0
@@ -160,7 +161,7 @@
 #define CPX_TASK_NAME             "CPX"
 #define APP_TASK_NAME             "APP"
 #define FLAPPERDECK_TASK_NAME     "FLAPPERDECK"
-
+#define TEENSY_TASK_NAME        "TEENSY"
 
 //Task stack sizes
 #define SYSTEM_TASK_STACKSIZE           (2* configMINIMAL_STACK_SIZE)
@@ -207,6 +208,7 @@
 #define KALMAN_TASK_STACKSIZE           (3 * configMINIMAL_STACK_SIZE)
 #define FLAPPERDECK_TASK_STACKSIZE      (2 * configMINIMAL_STACK_SIZE)
 #define ERROR_UKF_TASK_STACKSIZE        (4 * configMINIMAL_STACK_SIZE)
+#define TEENSY_TASK_STACKSIZE           (4 * configMINIMAL_STACK_SIZE)
 
 //The radio channel. From 0 to 125
 #define RADIO_CHANNEL 80
