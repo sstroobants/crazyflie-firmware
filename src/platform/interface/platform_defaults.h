ce763f8830192d572116d2a53cc88660a8c7538d
sstroobants <14180440+sstroobants@users.noreply.github.com>
2025-08-28 11:39:44 +0200

    include rotational component of drag (still needs testing to verify performance)

--- src/platform/interface/platform_defaults.h
+++ src/platform/interface/platform_defaults.h
@@ -230,3 +230,15 @@
 #ifndef EKF_DRAG_BY
     #define EKF_DRAG_BY 1.8f
 #endif
+#ifndef EKF_DRAG_BZ
+    #define EKF_DRAG_BZ 0.9f
+#endif
+#ifndef EKF_DRAG_RX
+    #define EKF_DRAG_RX 0.0f
+#endif
+#ifndef EKF_DRAG_RY
+    #define EKF_DRAG_RY 0.0f
+#endif
+#ifndef EKF_DRAG_RZ
+    #define EKF_DRAG_RZ 0.11f
+#endif
